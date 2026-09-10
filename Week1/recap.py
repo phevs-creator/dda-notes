9533aaba9922b6614a46114495255dd064dfb5ec
phevs-creator <phoebej297@gmail.com>
2026-09-10 16:16:52 +0100

    coding work

---
+++ Week1/recap.py
@@ -0,0 +1,51 @@
+# start
+
+def search(a, target):
+    # alternate solution - cuts down time by removing flag
+    # ans = "Not Found"
+    not_found = True
+
+    for val in a:
+        if val == target:
+            print("Found")
+            not_found = False
+            # ans = "Found"
+            break
+    
+    # print(ans)
+    if not_found:
+        print("Not Found")
+
+def add_pairs(a):
+    left = 0
+    right = len(a)-1
+
+    while left < right:
+        print(a[left] + a[right])
+        left = left + 1
+        right = right - 1
+
+        if a[left] == a[right]:
+            print(a[left])
+
+def binary_search(a, target):
+    val1 = 0
+    val2 = len(a)
+
+    for num in a:
+        mid = (val2+val1)//2
+
+        if a[mid] > target:
+            val2 = mid
+        elif a[mid] < target:
+            val1 = mid
+        else:
+            print("found")
+
+
+
+# this is to test the search function
+a = [1, 3, 5, 7, 11, 15, 23]
+target = 7
+
+binary_search(a, target)
